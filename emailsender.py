0eca20c7e04210ef6d71dec0714a759387af71c9
barandaban@gmail.com <barandaban@gmail.com>
2024-05-05 16:46:05 +0200

    First Commit

---
+++ emailsender.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import win32com.client as win32
+import pandas as pd
+from datetime import datetime
+
+
+def get_receiver(index, emails_list):
+    to = emails_list['Email'].loc[index]
+    salutation = str(emails_list['Salutation'].loc[index])
+    
+    month = int(emails_list['Month'].loc[index])
+    day = int(emails_list['Day'].loc[index])
+    hour = int(emails_list['Hour'].loc[index]) +2
+    minutes = int(emails_list['Minutes'].loc[index])
+    delay_date = datetime(2024, month, day, hour, minutes)
+    receiver = {'to': to, 'salutation': salutation, 'date': delay_date}
+    return receiver
+
+
+def read_message():
+     with open("C:/Users/Baran/Desktop/email/body.html", 'r') as message:
+        message_body = message.read()
+        return message_body
+
+
+def send_email(subject, to, body, date):
+    outlook = win32.Dispatch('Outlook.Application')
+    mail = outlook.CreateItem(0)
+    mail.Subject = subject
+    mail.To = to
+    mail.HTMLBody = body
+    mail.DeferredDeliveryTime = date
+    mail.Send()
+
---
+++ my-emailsender.py
@@ -0,0 +1,21 @@
+
+from emailsender import *
+
+emails_list = pd.read_excel('email_to_send.xlsx', dtype=str)
+SUBJECT = "Introducing Sterk Engineering"
+
+i = int(input('Start Line\n(Zero for First Line): '))
+n = int(input('End Line\n(Will be included): '))
+for i in range(i, n+1):
+        receiver_info = get_receiver(i, emails_list)
+        print(type(receiver_info))
+
+        content = read_message()
+        content = content.replace('Salutation', receiver_info['salutation'])
+        print(content)
+
+else:
+     print(f"emails_list between {i} and {n+1} are sent!")
+# end_time = datetime.now()
+# time_elapsed = end_time - start
+# print(time_elapsed, 'sec')
