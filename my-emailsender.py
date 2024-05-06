84b4da2bfe74404439f218a3c51c5ac4b727d50b
barandaban@gmail.com <barandaban@gmail.com>
2024-05-06 21:28:25 +0200

    afadf

--- my-emailsender.py
+++ my-emailsender.py
@@ -8,19 +8,22 @@ emails_list = pd.read_excel(path, dtype=str)
 SUBJECT = "Introducing Sterk Engineering"
 
 i = int(input('Start Line\n(Zero for First Line): '))
+j = [i]
+k = j[:]
 n = int(input('End Line\n(Will be included): '))
-for i in range(i, n+1):
+for i in range(i, n):
         receiver_info = get_receiver(i, emails_list)
         print(receiver_info)
+        print(i)
 
         content = read_message()
         content = content.replace('Salutation', receiver_info['salutation'])
-        send_email(SUBJECT, receiver_info['to'], content, receiver_info['date'])
+        #send_email(SUBJECT, receiver_info['to'], content, receiver_info['date'])
 
 else:
-     print(f"emails_list between {i} and {n+1} are sent!")
+     print(f"emails_list between {k} and {n} are sent!")
 
 #End Process Timer
 end = time.time()
 #Time elapsed
-print(end-start)
+print(end-start, 'second')
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+st['Email'].loc[index]
+    salutation = s
